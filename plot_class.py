d920770435d0043aa4a5d45796b2a1eb97ca5aca
mokon <michalionis123@gmail.com>
2022-01-11 20:33:31 +0100

    Remove code duplication from plotting

--- plot_class.py
+++ plot_class.py
@@ -20,24 +20,22 @@ def plot_dispersion_vector(n_points=10):
     plt.show()
 
 
-def plot_dispersion_x(n_points=1000):
+def plot_dispersion(n_points=1000, axis="x"):
     x, y = np.meshgrid(np.linspace(-1, 1, n_points), np.linspace(-1, 1, n_points))
-    dispersion_x = 1 + np.cos(np.pi * x)
-    plt.imshow(dispersion_x, extent=[-1, 1, -1, 1], origin='lower')
+    if axis == "x":
+        dispersion_x = 1 + np.cos(np.pi * x)
+        plt.imshow(dispersion_x, extent=[-1, 1, -1, 1], origin='lower')
+        plt.title("$D_x$")
+    elif axis == "y":
+        dispersion_y = 1 + np.cos(np.pi * y)
+        plt.imshow(dispersion_y, extent=[-1, 1, -1, 1], origin='lower')
+        plt.title("$D_y$")
     plt.colorbar()
-    plt.title("$D_x$")
     plt.show()
 
-def plot_dispersion_y(n_points=1000):
-    x, y = np.meshgrid(np.linspace(-1, 1, n_points), np.linspace(-1, 1, n_points))
-    dispersion_y = 1 + np.cos(np.pi * y)
-    plt.imshow(dispersion_y, extent=[-1, 1, -1, 1], origin='lower')
-    plt.colorbar()
-    plt.title("$D_y$")
-    plt.show()
 
 if __name__ == "__main__":
     plot_velocity_vector_field()
     plot_dispersion_vector()
-    plot_dispersion_x()
-    plot_dispersion_y()
+    plot_dispersion(axis="x")
+    plot_dispersion(axis="y")
