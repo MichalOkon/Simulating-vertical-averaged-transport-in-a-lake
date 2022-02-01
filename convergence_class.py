bde21ada2289f6a796b3c798aa0556b2979c00a4
janstruzinski <31801358+janstruzinski@users.noreply.github.com>
2022-02-01 23:05:52 +0100

    Fix (?) convergence_class.py

--- convergence_class.py
+++ convergence_class.py
@@ -13,22 +13,16 @@ class Convergence:
         self.y0 = y0
         self.scheme = scheme
 
-    def calculate_strong_error(self, x_log, y_log, x_accurate, y_accurate):
-        strong_error_x = np.mean(np.abs(x_log - x_accurate), axis=1)
-        strong_error_y = np.mean(np.abs(y_log - y_accurate), axis=1)
-        strong_error_xy = np.stack((strong_error_x, strong_error_y), axis=1)
-        print(strong_error_xy)
-        strong_error = np.linalg.norm(strong_error_xy, axis=1)
+    @staticmethod
+    def calculate_strong_error(xy, xy_accurate):
+        strong_error = np.linalg.norm(xy - xy_accurate)
+        print(strong_error)
         return strong_error
 
-    def calculate_weak_error(self, x_log, y_log, x_accurate, y_accurate):
-        weak_error_x = np.mean(np.abs(x_log), axis=1) - np.mean(np.abs(x_accurate))
-        weak_error_y = np.mean(np.abs(y_log), axis=1) - np.mean(np.abs(y_accurate))
-        print("mean x", np.mean(np.abs(x_log), axis=1))
-        print("mena x accurate", np.mean(np.abs(x_accurate)))
-        weak_error_xy = np.stack((weak_error_x, weak_error_y), axis=1)
-        weak_error = np.linalg.norm(weak_error_xy, axis=1)
-
+    @staticmethod
+    def calculate_weak_error(xy, xy_accurate):
+        weak_error = np.linalg.norm(xy) - np.linalg.norm(xy_accurate)
+        print(weak_error)
         return weak_error
 
     def estimate_convergence(self, n_samples, dt_ratio):
@@ -36,31 +30,21 @@ class Convergence:
         sim_accurate = sim.Sim(self.n_particles, self.dt * dt_ratio, self.x0, self.y0, self.t_end,
                                self.scheme)
         xy_accurate = np.reshape(sim_accurate.simulate()[1], (1, -1))
-        xy_log = np.zeros((n_samples, 2 * self.n_particles))
         dt_log = np.zeros(n_samples)
+        weak_error_log = np.zeros(n_samples)
+        strong_error_log = np.zeros(n_samples)
         for i in range(0, n_samples):
-            print(f"Running convergence simulation {i + 1}")
+            print(f"Running convergence simulation {i + 2}")
             dt = self.dt * (i + 1) / n_samples
             dummy_sim = sim.Sim(self.n_particles, dt, self.x0, self.y0, self.t_end, self.scheme)
             position_data, last_position = dummy_sim.simulate()
-            xy_log[i] = last_position
+            strong_error_log[i] = self.calculate_strong_error(last_position, xy_accurate)
+            weak_error_log[i] = self.calculate_weak_error(last_position, xy_accurate)
             dt_log[i] = dt
 
-        # print(xy_accurate)
-        # print(xy_log)
-        x_accurate = xy_accurate[0, : int(xy_accurate.shape[1] / 2)]
-        y_accurate = xy_accurate[0, int(xy_accurate.shape[1] / 2):]
-        x_log = xy_log[:, : int(xy_log.shape[1] / 2)]
-        y_log = xy_log[:, int(xy_log.shape[1] / 2):]
-        print(x_accurate)
-        print(x_log)
-        print(f"absolute = {np.abs(x_log - x_accurate)}")
-        weak_error = self.calculate_weak_error(x_log, y_log, x_accurate, y_accurate)
-        strong_error = self.calculate_strong_error(x_log, y_log, x_accurate, y_accurate)
-
-        plot_error(weak_error, dt_log, self.scheme, self.t_end, "Weak")
-        plot_error(strong_error, dt_log, self.scheme, self.t_end, "Strong")
+        plot_error(weak_error_log, dt_log, self.scheme, self.t_end, "Weak")
+        plot_error(strong_error_log, dt_log, self.scheme, self.t_end, "Strong")
 
         # print(weak_error)
         # print(strong_error)
-        return strong_error, dt_log
+        return strong_error_log, weak_error_log, dt_log
--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import sim_class as sim
 
 #sim = sim.Sim(10, 1e-4, 0.5, 0.5, 1e2, "Euler")
 #xy = sim.simulate()
-convergence_analysis = conv.Convergence(n_particles=5000, dt=1e-3, x0=0.5, y0=0.5, t_end=1,
+convergence_analysis = conv.Convergence(n_particles=5000, dt=0.001, x0=0.5, y0=0.5, t_end=5,
                                         scheme="Euler")
-error, dt = convergence_analysis.estimate_convergence(10, 0.01)
+error, dt = convergence_analysis.estimate_convergence(10, 0.001)
